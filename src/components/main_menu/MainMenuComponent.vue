54a59a4fbff1a8aeb3e7d8c696087b477964b6d5
fwmakc <fwmakcpro@yandex.ru>
2026-03-11 16:23:42 +0300

    modified:   src/components/main_menu/MainMenuComponent.vue

--- src/components/main_menu/MainMenuComponent.vue
+++ src/components/main_menu/MainMenuComponent.vue
@@ -19,7 +19,6 @@
 </template>
 
 <script setup lang="ts">
-import { useQuasar } from 'quasar';
 import { useI18n } from 'vue-i18n';
 import { useRoute } from 'vue-router';
 
@@ -29,8 +28,4 @@ const { t } = useI18n();
 const route = useRoute();
 
 const active = (name: string): boolean => route.matched[2]?.name === name;
-
-const $q = useQuasar();
-
-const mobile = (name: string): boolean => route.matched[2]?.name === name;
 </script>
